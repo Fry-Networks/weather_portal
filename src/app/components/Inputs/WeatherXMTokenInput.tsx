1e05444a607f74707581bac9860e825bb29095b7
Simon <44496148+siim0n@users.noreply.github.com>
2024-01-08 19:45:28 +0100

    remove logs

--- src/app/components/Inputs/WeatherXMTokenInput.tsx
+++ src/app/components/Inputs/WeatherXMTokenInput.tsx
@@ -25,8 +25,6 @@ const WeatherXMTokenInput = ({
     data-form-type="other"
     onChange={(e) => {
       setToken(e.target.value);
-      console.log(e.target.value);
-      console.log(/\S+@\S+\.\S+/g.test(e.target.value));
       type === 'email'
         ? setValid(/\S+@\S+\.\S+/g.test(e.target.value))
         : setValid(e.target.value !== '');
